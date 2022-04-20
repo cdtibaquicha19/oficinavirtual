8398620ca7d2b3f6b882433dffb4e160caf35c33
Cristyan Tibaquicha <cdtibaquicha19@gmail.com>
2022-04-20 16:33:47 -0500

    Carga configuración

--- hms/include/config.php
+++ hms/include/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hms');
+define('DB_USER','samuaeve_rootofi');
+define('DB_PASS' ,'7M3])k,MR2pr');
+define('DB_NAME', 'samuaeve_oficina');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
