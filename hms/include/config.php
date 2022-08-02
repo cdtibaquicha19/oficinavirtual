aae4d52836227f5704339e78348e1389d891ed26
Cristyan Tibaquicha <50309035+cdtibaquicha19@users.noreply.github.com>
2022-08-02 11:29:20 -0500

    Update config.php

--- hms/include/config.php
+++ hms/include/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','samuaeve_rootofi');
-define('DB_PASS' ,'7M3])k,MR2pr');
-define('DB_NAME', 'samuaeve_oficina');
+define('DB_USER','');
+define('DB_PASS' ,'');
+define('DB_NAME', '');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
